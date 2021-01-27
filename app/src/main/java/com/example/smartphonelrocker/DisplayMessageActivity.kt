ddb49ebdb6d77ceb0dd7c38a96ca5237c52ecdbf
lasurstein <sabamoffy.1258@gmail.com>
2021-01-28 00:47:48 +0900

    10 second alarm

--- app/src/main/java/com/example/smartphonelrocker/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/smartphonelrocker/DisplayMessageActivity.kt
@@ -13,12 +13,6 @@ class DisplayMessageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
 
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
-
-        val textView = findViewById<TextView>(R.id.textView).apply {
-            text = message
-        }
-
         mp0= MediaPlayer.create(this,R.raw.bgm_maoudamashii_neorock80)
         mp0.isLooping=false
 
--- app/src/main/java/com/example/smartphonelrocker/MainActivity.kt
+++ app/src/main/java/com/example/smartphonelrocker/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.smartphonelrocker
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -7,7 +9,7 @@ import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.EditText
-import android.media.MediaPlayer
+import java.util.*;
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,12 +21,26 @@ class MainActivity : AppCompatActivity() {
 
     fun sendMessage(view: View) {
 
-
         val editText = findViewById<EditText>(R.id.editText)
         val message = editText.text.toString()
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
-        }
-        startActivity(intent)
+
+        val editTime = findViewById<EditText>(R.id.editTime)
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val calendar = Calendar.getInstance()
+        val intent = Intent(this, MyReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        calendar.add(Calendar.SECOND, 10)
+        alarmManager.setExact(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            pendingIntent
+        )
+
+//        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+//            putExtra(EXTRA_MESSAGE, message)
+//        }
+//        startActivity(intent)
     }
 }
